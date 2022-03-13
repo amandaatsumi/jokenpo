36c04f7e9921854993a483a773ba20fa3e89d2e1
AAtsumi <59661487+amandaatsumi@users.noreply.github.com>
2022-03-13 19:32:21 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -9,3 +9,26 @@ var imageSource2 = "images/img" + randomNumber2 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", imageSource1);
 
 document.querySelectorAll("img")[1].setAttribute("src", imageSource2);
+
+if(randomNumber1=== 1 & randomNumber2 === 2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!!";
+}
+else if(randomNumber1=== 1 & randomNumber2 === 3){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!!";
+}
+else if(randomNumber1=== 2 & randomNumber2 === 1){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!!";
+}
+else if(randomNumber1=== 2 & randomNumber2 === 3){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!!";
+}
+else if(randomNumber1=== 3 & randomNumber2 === 1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!!";
+}
+else if(randomNumber1=== 3 & randomNumber2 === 2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!!";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
+
