862a7f94d8875fd64dc6144c92958aa6161d11da
AAtsumi <59661487+amandaatsumi@users.noreply.github.com>
2022-03-14 21:03:16 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -11,37 +11,37 @@ document.querySelectorAll("img")[0].setAttribute("src", imageSource1);
 document.querySelectorAll("img")[1].setAttribute("src", imageSource2);
 
 if(randomNumber1=== 1 & randomNumber2 === 2) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins!! 🏆";
+    document.querySelector("h1.refresh").innerHTML = "Player 2 Wins!! 🏆";
     document.querySelector("button").innerHTML = "Play Again";
     
 }
 else if(randomNumber1=== 1 & randomNumber2 === 3){
-    document.querySelector("h1").innerHTML = "🏆 Player 1 Wins!!";
+    document.querySelector("h1.refresh").innerHTML = "🏆 Player 1 Wins!!";
     document.querySelector("button").innerHTML = "Play Again";
 
 }
 else if(randomNumber1=== 2 & randomNumber2 === 1){
-    document.querySelector("h1").innerHTML = "🏆 Player 1 Wins!!";
+    document.querySelector("h1.refresh").innerHTML = "🏆 Player 1 Wins!!";
     document.querySelector("button").innerHTML = "Play Again";
     
 }
 else if(randomNumber1=== 2 & randomNumber2 === 3){
-    document.querySelector("h1").innerHTML = "Player 2 Wins!! 🏆";
+    document.querySelector("h1.refresh").innerHTML = "Player 2 Wins!! 🏆";
     document.querySelector("button").innerHTML = "Play Again";
     
 }
 else if(randomNumber1=== 3 & randomNumber2 === 1){
-    document.querySelector("h1").innerHTML = "Player 2 Wins!! 🏆";
+    document.querySelector("h1.refresh").innerHTML = "Player 2 Wins!! 🏆";
     document.querySelector("button").innerHTML = "Play Again";
     
 }
 else if(randomNumber1=== 3 & randomNumber2 === 2){
-    document.querySelector("h1").innerHTML = "🏆 Player 1 Wins!!";
+    document.querySelector("h1.refresh").innerHTML = "🏆 Player 1 Wins!!";
     document.querySelector("button").innerHTML = "Play Again";
     
 }
 else{
-    document.querySelector("h1").innerHTML = "Draw";
+    document.querySelector("h1.refresh").innerHTML = "Draw";
     document.querySelector("button").innerHTML = "Play Again";
     
 }
